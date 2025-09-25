3a4e87e20b37e264a5c54733c08b96e232f46d25
Neptoki <zoedn1911@gmail.com>
2025-09-25 19:40:14 +0100

    camera implemented test?

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCamera
+public class PlayerCamera : MonoBehaviour
 {
     public float sensX;
     public float sensY;
