f8d53ab3fe149fba5864ed1cb7598586b18a4580
Neptoki <zoedn1911@gmail.com>
2025-11-04 21:12:37 +0000

    made the item spin and float

--- Assets/Scripts/HealingItem.cs
+++ Assets/Scripts/HealingItem.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
 public class HealingItem : MonoBehaviour
 {
+    [SerializeField] private Vector3 rotationSpeed = new Vector3(0, 100, 0);
+    [SerializeField] private float floatAmplitude = 0.25f;
+    [SerializeField] private float floatFrequency = 2f;
     public float healAmount = 5f;
+    private Vector3 startPos;
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -9,5 +14,18 @@ public class HealingItem : MonoBehaviour
             other.GetComponent<PlayerStats>().HealPlayer(healAmount);
             Destroy(gameObject);
         }
+    }
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        //spin and float
+        transform.Rotate(rotationSpeed * Time.deltaTime);
+        float newY = startPos.y + Mathf.Sin(Time.time * floatFrequency) * floatAmplitude;
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
     }
 }
